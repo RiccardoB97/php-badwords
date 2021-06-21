7a30e44e712fd5ce8ea2acc9cf54fe31da5f479b
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-21 13:54:11 +0200

    Add variable paragraph

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+   $paragraph = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ipsum perspiciatis ad dolore nisi repudiandae rerum, quam sed iure in!";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+</body>
+</html>
